b589882eb5768326372299704116d63194ecf221
Bruno Santos <bcs.2707@gmail.com>
2024-11-26 10:21:06 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
     viewportHeight: 720,
-    //specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
     specPattern: 'cypress/e2e/**/*.cy.js', // Caminho dos testes
     supportFile: false, // Ajuste conforme necessário
   }
