fffdcc6d334e53dd2f2e598335a816cb0c4ccc63
bruno santos <bruno.csantos@salvador.ba.gov.br>
2024-11-26 10:43:33 -0300

    Alterações no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
     viewportHeight: 720,
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     //specPattern: 'cypress/e2e/**/*.cy.js', // Caminho dos testes
     supportFile: false, // Ajuste conforme necessário
   }
